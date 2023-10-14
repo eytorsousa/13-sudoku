a6f6848c1d35428be027679922970d51f607f579
eytorlima <eytorlima@outlook.com>
2023-10-13 21:08:06 -0300

    adding functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,25 @@
 const urlParams = new URLSearchParams(window.location.search);
 const value = urlParams.get('rxc');
-console.log(value);
+
+const game = document.querySelector('.game');
+
+
+game.style.width = `calc(74px * ${value})`;
+
+
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+for(let i = 0; i < value; i++){
+    for(let j = 0; j < value; j++){
+        let block = document.createElement('input');
+        block.type = 'number';
+        block.max = '9';
+        block.min = '1';
+        block.classList.add('block');
+        block.classList.add(`${letters[i]}`);
+        block.classList.add(`${numbers[j]}`);
+        game.appendChild(block);
+    }
+}
+
