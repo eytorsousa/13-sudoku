ebc023ae088ae7176b47d312fd2540327337bcc7
Eytor Lima <eytorlima@outlook.com>
2023-10-17 15:47:52 -0300

    organizing code

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,15 @@
 const urlParams = new URLSearchParams(window.location.search);
 const value = urlParams.get('rxc');
 const verify = document.querySelector('.verify');
-var blocks;
-var rows = [];
-var cols = [];
-
 const game = document.querySelector('.game');
 game.style.width = `calc(74px * ${value})`;
 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var blocks;
+var rows = [];
+var cols = [];
 
 for(let i = 0; i < value; i++){
     for(let j = 0; j < value; j++){
@@ -22,13 +22,20 @@ for(let i = 0; i < value; i++){
     }
 }
 
-blocks = Array.from(document.querySelectorAll('.block'));
-for(let i = 0; i < value; i++){
-    rows[i] = Array.from(document.querySelectorAll(`.${letters[i]}`));
-    cols[i] = Array.from(document.querySelectorAll(`.n${numbers[i]}`));
-}
-
+createArrays();
 setvalues();
+
+setInterval(() => {
+    blocks.forEach(element => {
+        if(element.value < 1 && element.value != ''){
+            element.value = 1;
+        } else if(element.value > value && element.value != ''){
+            element.value = value;
+        } else if(element.value.length != 1){
+            element.value = '';
+        }
+    });
+}, 100);
 
 verify.addEventListener('click', () => {
     var win;
@@ -39,6 +46,26 @@ verify.addEventListener('click', () => {
         window.alert('Parabéns, você venceu!');
     }
 });
+
+function createArrays(){
+    blocks = Array.from(document.querySelectorAll('.block'));
+    for(let i = 0; i < value; i++){
+        rows[i] = Array.from(document.querySelectorAll(`.${letters[i]}`));
+        cols[i] = Array.from(document.querySelectorAll(`.n${numbers[i]}`));
+    }
+}
+
+function setvalues(){
+    rows.forEach(row => {
+        let position = Math.floor(Math.random() * value);
+        let num = Math.floor(Math.random() * value);
+        for(let i = 0; i < value; i++){
+            row[position].disabled = true;
+            row[position].value = num;
+            row[position].classList.add('blocked');
+        }
+    });
+}
 
 function verifyInputs(win){
     for(let i = 0; i < blocks.length; i++){
@@ -77,29 +104,4 @@ function verifyRC(array){
     } else {
         return true;
     }
-}
-
-setInterval(() => {
-    blocks.forEach(element => {
-        if(element.value < 1 && element.value != ''){
-            element.value = 1;
-        } else if(element.value > value && element.value != ''){
-            element.value = value;
-        } else if(element.value.length != 1){
-            element.value = '';
-        }
-    });
-}, 100);
-
-
-function setvalues(){
-    rows.forEach(row => {
-        let position = Math.floor(Math.random() * value);
-        let num = Math.floor(Math.random() * value);
-        for(let i = 0; i < value; i++){
-            row[position].disabled = true;
-            row[position].value = num;
-            row[position].classList.add('blocked');
-        }
-    });
 }
